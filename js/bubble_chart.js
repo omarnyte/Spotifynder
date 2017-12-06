2e21a1d95fcab591cd6b25635b35189bf7accd64
Omar <omar.dlsg@gmail.com>
2017-12-05 19:56:14 -0800

    Makes static bubbles appear on index.html

---
+++ js/bubble_chart.js
@@ -0,0 +1,12 @@
+// function bubbleChart() {
+//   var width = 1200;
+//   var height = 700;
+//
+//   // var center = { x: width / 2, y: height / 2};
+//
+//   var svg = null;
+//   var bubbles = null;
+//   var nodes = [];
+//
+//
+// }
