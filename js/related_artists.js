598d2fa37c99565f028073d0e119e1effa617015
Omar <omar.dlsg@gmail.com>
2017-12-06 13:00:19 -0800

    Resizes each related artist's thumbnail

--- js/related_artists.js
+++ js/related_artists.js
@@ -886,11 +886,21 @@ relatedArtistsDiv.appendChild(h1);
 
 const ul = document.createElement("ul");
 relatedArtistsDiv.appendChild(ul);
-artistNames.forEach(function(name, idx) {
-  const li = document.createElement("li");
-  li.textContent = name;
-  li.className = 'related-artist-name';
-  ul.appendChild(li);
+
+testJSONResp.artists.forEach(function(artist, idx) {
+  const div = document.createElement("div");
+  div.className = 'related-artists-item-div';
+  relatedArtistsDiv.appendChild(div);
+
+  const img = document.createElement("img");
+  img.className = 'related-artist-thumbnail';
+  img.src = selectImageThumbnail(artist.images);
+  div.appendChild(img);
+
+  const span = document.createElement("span");
+  span.textContent = artist.name;
+  span.className = 'related-artist-names';
+  div.appendChild(span);
 });
 
 // Selects the first image whose height/width ratio is 1/1. Otherwise,
