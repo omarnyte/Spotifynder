cd9a9caa956263a3d22194ae971b6d2117dadbc4
Omar <omar.dlsg@gmail.com>
2017-12-22 06:39:56 -0800

    Style intro page

--- index.js
+++ index.js
@@ -6,3 +6,22 @@ const spotifyAuth = require('./lib/spotify-auth');
 
 const app = express();
 const port = process.env.PORT || 8080;
+
+app.use(express.static(__dirname + '/'))
+   .use(cookieParser());
+
+let token;
+
+if (process.env.PORT) {
+  port = process.env.PORT;
+}
+
+spotifyAuth((err, resp, body) => {
+  token = body.access_token;
+  console.log(`Listening on ${port}`);
+  app.listen(port);
+});
+
+app.get('/callback', (req, res) => {
+  res.send(token);
+});
