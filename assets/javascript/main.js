344aa2f144740e1a90921d37ee87fb3db25428c2
Omar <omar.dlsg@gmail.com>
2017-12-22 08:24:52 -0800

    On search submit, fetch query results

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -23,26 +23,16 @@ searchForm.addEventListener("submit", e => {
 
   const charts = document.querySelector(".charts");
   charts.classList.remove("hidden");
-});
 
-// document.addEventListener("DOMContentLoaded", function(event) {
-//   const searchForm = document.querySelector(".search-form");
-//
-//   searchForm.addEventListener("submit", e => {
-//     e.preventDefault();
-//
-//     const searchQuery = document.querySelector(".search-bar");
-//     console.log(searchQuery.value);
-//     const artistID = searchQuery.value;
-//     searchQuery.value = "";
-//     // s.searchArtists(searchQuery.value)
-//     //   .then(resp => console.log(resp));
-//
-//     s.getArtistRelatedArtists('00FQb4jTyendYWaN8pK0wa') //TODO change back to name
-//       .then(resp => {
-//         console.log(resp);
-//         // new bubbleChart();
-//         new relatedArtists(resp);
-//     });
-//   });
-// });
+  // spotify search query
+  const searchQuery = document.querySelector('.search-bar').value;
+  spotify.searchArtists(searchQuery)
+    .then(artistResp => {
+      const artistId = artistResp.artists.items[0].id;
+      spotify.getArtistRelatedArtists(artistId)
+        .then(relatedArtistResp => {
+          console.log(relatedArtistResp);
+        }); 
+    });
+
+});
