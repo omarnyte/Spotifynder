f90501ba4594b2026d3967c77c8f750795dc7eaa
Omar <omar.dlsg@gmail.com>
2017-12-23 15:05:05 -0800

    Add event listeners for clicks on search result items

--- assets/javascript/bundle.js
+++ assets/javascript/bundle.js
@@ -24621,39 +24621,37 @@ function fetchMatches(e) {
 
 function displayMatches(queryResults) {
   const artistResults = queryResults.artists.items;
-  console.log(artistResults);
 
   const html = artistResults.map(artistObject => {
     const name = artistObject.name;
     const id = artistObject.id;
-    console.log(name, id);
     return `
-      <li class='query-item' data-artistId=${id}>
-        <span class="query-item-name">${name}</span>
+      <li class='search-result-item' data-artistId=${id}>
+        <span class="search-result-item-name">${name}</span>
       </li>
   `;
-}).join('');
-suggestions.innerHTML = html;
+  }).join('');
+  suggestions.innerHTML = html;
+
+  appendEventListeners();
 }
 
-// function displayMatches() {
-//   const matchArray = findMatches(this.value, cities);
-//   const html = matchArray.map(place => {
-//     const regex = new RegExp(this.value, 'gi');
-//     const cityName = place.city.replace(regex, `<span class="hl">${this.value}</span>`);
-//     const stateName = place.state.replace(regex, `<span class="hl">${this.value}</span>`);
-//     return `
-//       <li>
-//         <span class="name">${cityName}, ${stateName}</span>
-//         <span class="population">${numberWithCommas(place.population)}</span>
-//       </li>
-//     `;
-//   }).join('');
-//   suggestions.innerHTML = html;
-// }
-//
-// const searchInput = document.querySelector('.search');
-// const suggestions = document.querySelector('.suggestions');
+function appendEventListeners() {
+  const searchResultItems = document.querySelectorAll('.search-result-item');
+  console.log(searchResultItems);
+  searchResultItems.forEach(item => {
+    item.addEventListener('click', createCharts);
+  });
+}
+
+function createCharts(e) {
+  console.log('clicked!');
+}
+
+
+// const keys = Array.from(document.querySelectorAll('.key'));
+// keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+// window.addEventListener('keydown', playSound);
 
 
 /***/ })
