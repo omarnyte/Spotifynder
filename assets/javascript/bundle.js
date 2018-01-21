6d370e218ef58340715081c3fa1ec3654474be79
Omar <omar.dlsg@gmail.com>
2018-01-21 15:45:03 -0800

    Render chart.js chart on page

--- assets/javascript/bundle.js
+++ assets/javascript/bundle.js
@@ -24779,12 +24779,47 @@ function nopropagation() {
 
 "use strict";
 class Graph {
-  constructor(relatedArtistsResp) {
-    console.log(relatedArtistsResp);
-    this.countGenres(relatedArtistsResp);
+  constructor(relatedArtists) {
+    // console.log('related artists', relatedArtists);
+    this.render(relatedArtists);
   }
 
   render(relatedArtists) {
+    this.populateGraph(relatedArtists);
+  }
+
+  populateGraph(relatedArtists) {
+    let genreCount = this.countGenres(relatedArtists);
+    console.log('genre count', genreCount);
+
+    // const graph = document.querySelector('.graph');
+    // Object.keys(genreCount).forEach(genre => {
+    //   var bar = document.createElement('rect');
+    //   bar.setAttribute('width', '500');
+    //   bar.setAttribute('height', '500');
+    //   // bar.setAttribute('fill', '#'+Math.round(0xffffff * Math.random()).toString(16));
+    //   bar.style.fill = 'rgb(0,0,255)';
+    //   graph.appendChild(bar);
+    // });
+
+    var ctx = document.querySelector('.graph').getContext('2d');
+    var myBarChart = new Chart(ctx, {
+      type: 'bar',
+
+   // The data for our dataset
+   data: {
+       labels: Object.keys(genreCount),
+       datasets: [{
+           label: "My First dataset",
+           backgroundColor: 'rgb(255, 99, 132)',
+           borderColor: 'rgb(255, 99, 132)',
+           data: Object.values(genreCount),
+       }]
+   },
+
+   // Configuration options go here
+   options: {}
+    });
   }
 
   countGenres(relatedArtists) {
@@ -24799,7 +24834,8 @@ class Graph {
         }
       });
     });
-    console.log(genreCounter);
+
+    // console.log(genreCounter);
     return genreCounter;
   }
 
