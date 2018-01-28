37ac5f5fdf1e6eacb3e1c6387e8e499862935bcf
Omar <omar.dlsg@gmail.com>
2018-01-27 20:44:23 -0800

    Enable voice search

--- assets/javascript/bundle.js
+++ assets/javascript/bundle.js
@@ -11615,22 +11615,23 @@ function record() {
   recording = true;
   mic.classList.add('recording');
   console.log('recording', recording);
-  // window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+
+  window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
   //
-  // const recognition = new SpeechRecognition();
-  // recognition.interimResults = true;
-  //
-  // recognition.start();
-  //
-  // recognition.addEventListener('result', e => {
-  //   const transcript = Array.from(e.results)
-  //     .map(result => result[0])
-  //     .map(result => result.transcript)
-  //     .join('');
-  //
-  //     searchBar.value = 'teeeest';
-  //     console.log(transcript);
-  // });
+  const recognition = new SpeechRecognition();
+  recognition.interimResults = true;
+
+  recognition.start();
+
+  recognition.addEventListener('result', e => {
+    const transcript = Array.from(e.results)
+      .map(result => result[0])
+      .map(result => result.transcript)
+      .join('');
+
+      searchBar.value = transcript;
+      console.log(transcript);
+  });
 }
 
 function stopRecording() {
